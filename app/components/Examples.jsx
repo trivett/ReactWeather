63a7990c967d732448cc315cf8d8418cd0079b2c
Vincent Trivett <trivett@gmail.com>
2016-06-07 19:34:44 -0400

    added some foundation styles

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,7 +1,21 @@
 var React = require('react');
+var {Link} = require('react-router')
 
 var Examples = (props) => {
-  return <h3>Examples</h3>
+  return (
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>here are some example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Philadelphia'>Philadelphia</Link>
+        </li>
+        <li>
+          <Link to='/?location=Rio'>Rio, Brasil</Link>
+        </li>
+      </ol>
+    </div>
+  )
 }
 
 module.exports = Examples;
