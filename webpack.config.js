a53d05144c1321ec8d03f0636efef56b3a09c4be
Vincent Trivett <trivett@gmail.com>
2016-06-19 14:51:17 -0400

    sassy css

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
             openWeatherMap: 'app/api/openWeatherMap.jsx',
             Examples: 'app/components/Examples.jsx',
             ErrorModal: 'app/components/ErrorModal.jsx',
-            applicationStyles: 'app/styles/app.css'
+            applicationStyles: 'app/styles/app.scss'
         },
         extensions: ['', '.js', '.jsx']
     },
